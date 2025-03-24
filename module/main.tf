e046639e89274c280fe4f80e675cf1bdded470bd
Francklin Darryl <darrylnnon@gmail.com>
2025-03-24 18:38:03 +0000

    static website

--- module/main.tf
+++ module/main.tf
@@ -6,16 +6,59 @@ required_providers {
     }
   }
 }
-provider "aws" {
- # region     = "us-west-2"
- # access_key = "my-access-key"
- # secret_key = "my-secret-key"
+# S3 Bucket for Static Website Hosting
+resource "aws_s3_bucket" "static_site" {
+  bucket = "my-static-website-bucket"  # Change to a unique bucket name
 }
-resource "aws_s3_bucket" "website_bucket" {
-  
-  bucket = var.bucket_name
 
-  tags = {
-    UserUuid  = var.user_uuid
+# Enable Static Website Hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
   }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Bucket Policy to Allow Public Read Access
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.static_site.id}/*"
+    }
+  ]
+}
+EOF
+}
+
+# Upload Example HTML Files
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.static_site.id
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.static_site.id
+  key    = "error.html"
+  source = "error.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
